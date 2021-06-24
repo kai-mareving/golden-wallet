a309b062d053d273480a3aff0378eba5e62b7e4a
Kai Mareving <karska.joanna@gmail.com>
2021-06-24 22:26:11 +0200

    main section v.0.2

--- src/js/script.js
+++ src/js/script.js
@@ -6,12 +6,18 @@ const app = {
     datePicker.dom.minDate = document.querySelector('#minDate');
     datePicker.dom.maxDate = document.querySelector('#maxDate');
     datePicker.nowDate = new Date();
-    datePicker.maxDate = addDays(datePicker.nowDate, 14);
+    datePicker.maxDate = app.addDays(datePicker.nowDate, 14);
 
     // eslint-disable-next-line no-undef
     flatpickr(datePicker.dom.minDate,{defaultDate: datePicker.nowDate,});
     // eslint-disable-next-line no-undef
     flatpickr(datePicker.dom.maxDate,{defaultDate: datePicker.maxDate,});
+  },
+
+  addDays: function(dateStr, days) {
+    const dateObj = new Date(dateStr);
+    dateObj.setDate(dateObj.getDate() + days);
+    return dateObj;
   },
 
   init: function () {
@@ -22,9 +28,3 @@ const app = {
 };
 
 app.init();
-
-function addDays(dateStr, days) {
-  const dateObj = new Date(dateStr);
-  dateObj.setDate(dateObj.getDate() + days);
-  return dateObj;
-}
