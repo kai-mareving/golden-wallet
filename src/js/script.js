d793212b10d34d25fe96d229d780c9d47c254a12
Kai Mareving <karska.joanna@gmail.com>
2021-07-02 15:54:49 +0200

    renderChart() v.0.1

--- src/js/script.js
+++ src/js/script.js
@@ -79,7 +79,63 @@ const app = {
           thisTarget.style.display = 'none';
         }
       };
+
+      document.addEventListener('keyup', function (event) {
+        if (event.keyCode === 27) {
+          modal.style.display = 'none';
+        }
+      });
     }
+
+  },
+
+  renderChart: function () {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    // Chart.defaults.global.legend.labels.usePointStyle = true;
+
+    // eslint-disable-next-line no-undef
+    const chart = new Chart(ctx, {
+      // 1
+      type: 'bar',
+      data: {
+        // 2
+        labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+        // 3
+        datasets: [{
+          // 4
+          label: 'Signups',
+          // 5
+          backgroundColor: '#8DBEC8',
+          borderColor: '#8DBEC8',
+          // 6
+          data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+        },
+        {
+          label: 'FTD',
+          backgroundColor: '#F29E4E',
+          borderColor: '#F29E4E',
+          data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+        },
+        {
+          label: 'Earned',
+          backgroundColor: '#71B374',
+          borderColor: '#71B374',
+          data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+          // 7
+          hidden: true,
+        }]
+      },
+      options: {
+        legend: {
+          display: false,
+          position: 'top',
+          labels: {
+            fontColor: '#56819F',
+            padding: 20,
+          }
+        }
+      }
+    });
 
   },
 
@@ -89,6 +145,7 @@ const app = {
     thisApp.initDatePicker();
     thisApp.hideSidebar();
     thisApp.initModal();
+    thisApp.renderChart();
   }
 };
 
